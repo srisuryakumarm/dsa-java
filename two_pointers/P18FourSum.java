66cebac474dacacf3239dfbce51e83e72e9034fd
Sri Surya Kumar <srisuryaip@gmail.com>
2026-08-17 20:25:45 +0530

    Day 13 Two Pointers Revision & Code Corrections

--- two_pointers/P18FourSum.java
+++ two_pointers/P18FourSum.java
@@ -89,8 +89,8 @@ public class P18FourSum {
 
     // Optimised
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
@@ -99,22 +99,17 @@ public class P18FourSum {
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
-                int l = j + 1;
-                int r = nums.length - 1;
+                int l = j + 1, r = nums.length - 1;
                 while (l < r) {
                     long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
                     if (sum == target) {
-                        result.add(
-                                Arrays.asList(nums[i], nums[j], nums[l], nums[r])
-                        );
-                        while (l < r && nums[l] == nums[l + 1]) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[l++], nums[r--]));
+                        while (l < r && nums[l] == nums[l - 1]) {
                             l++;
                         }
-                        while (l < r && nums[r] == nums[r - 1]) {
+                        while (l < r && nums[r] == nums[r + 1]) {
                             r--;
                         }
-                        l++;
-                        r--;
                     } else if (sum > target) {
                         r--;
                     } else {
